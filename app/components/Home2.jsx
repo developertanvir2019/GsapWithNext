60ce5ea39df564809ecfd09805926f9db4f9efab
developertanvir2019 <developertanvir2019@gmail.com>
2024-01-18 23:39:22 +0600

    remove animation

---
+++ app/components/Home2.jsx
@@ -0,0 +1,9 @@
+const Home2 = () => {
+  return (
+    <div>
+      <h3>hi home 2</h3>
+    </div>
+  );
+};
+
+export default Home2;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Home1 from "./components/Home";
+import Home2 from "./components/Home2";
 
 export default function Home() {
   return (
     <main>
-      <Home1 />
+      {/* <Home1 /> */}
+      <Home2 />
     </main>
   );
 }
