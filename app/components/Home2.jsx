553d81f86d56adac65f5cfeac057d80d6cc50502
developertanvir2019 <developertanvir2019@gmail.com>
2024-01-19 17:26:14 +0600

    fix some issue for animation

--- app/components/Home2.jsx
+++ app/components/Home2.jsx
@@ -1,10 +1,12 @@
 "use client";
-import gsap from "gsap";
+import { gsap } from "gsap";
 import { useEffect, useRef } from "react";
 
 const Home2 = () => {
   let tl = gsap.timeline();
-  let cursor = useRef();
+  let tl2 = gsap.timeline();
+  let cursor = useRef(null);
+  let h1 = useRef(null);
   let posX = 0;
   let posY = 0;
   let mouseX = 0;
@@ -27,16 +29,37 @@ const Home2 = () => {
       mouseX = e.pageX;
       mouseY = e.pageY;
     });
+    // tl2.from(h1.children, {
+    //   skewY: 8,
+    //   y: 100,
+    //   duration: 0.3,
+    //   opacity: 0,
+    //   stagger: {
+    //     each: 0.4,
+    //     from: "end",
+    //   },
+    // });
+    // tl2.from(
+    //   cursor,
+    //   {
+    //     duration: 1.5,
+    //     delay: 1,
+    //     opacity: 0,
+    //   },
+    //   "-=1"
+    // );
   }, []);
   return (
-    <div className="home2 relative h-screen w-screen">
-      <h1 className="text-3xl font-bold py-2">Tanvir Ahmed</h1>
-      <h1 className="text-3xl font-bold py-2">Mern Stack developer</h1>
-      <h1 className="text-3xl font-bold py-2">GSAP Animation</h1>
+    <div className="h-96 w-screen overflow-hidden bg-[#121212]">
       <div
-        ref={(el) => (cursor = el)}
-        className="cursor-follow absolute h-20 w-20 top-0 left-0"
-      ></div>
+        // ref={(el) => (h1 = el)}
+        className="content2"
+      >
+        <h1 className="text-3xl font-bold py-2">Tanvir Ahmed</h1>
+        <h1 className="text-3xl font-bold py-2">Mern Stack developer</h1>
+        <h1 className="text-3xl font-bold py-2">GSAP Animation</h1>
+      </div>
+      <div ref={(el) => (cursor = el)} className="cursor-follow"></div>
     </div>
   );
 };
